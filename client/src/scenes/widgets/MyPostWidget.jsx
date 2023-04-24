472d8c5d5a9d0a230c1adfa1f1012c8084359f68
hem <akulahemanth307@gmail.com>
2023-04-24 00:19:07 -0600

    new commit

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -38,7 +38,7 @@ const MyPostWidget = ({picturePath}) => {
             formData.append("picturePath", image.name);
         }
 
-        const response = await fetch(`http://localhost:3001/posts`,{
+        const response = await fetch(`https://network-backend.onrender.com/posts`,{
             method: "POST",
             headers: {Authorization : `Bearer ${token}`},
             body: formData
